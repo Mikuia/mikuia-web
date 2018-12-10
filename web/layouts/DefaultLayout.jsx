883574d075e7399891fb82933e4119da713e95a7
Laura Lynn <laura@iys.io>
2018-12-10 14:32:50 +0100

    failure of a twitch login and basic page

--- web/layouts/DefaultLayout.jsx
+++ web/layouts/DefaultLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 
-import {Col, Container, Row} from 'reactstrap';
+import {Col, Container, Navbar, NavbarBrand, Nav, NavItem, NavLink, Row} from 'reactstrap';
 
 import Test from '../components/Test';
 
@@ -10,7 +10,7 @@ class DefaultLayout extends React.Component {
 		super();
 
 		this.state = {
-			error: true
+			error: false
 		}
 	}
 
@@ -32,7 +32,7 @@ class DefaultLayout extends React.Component {
 								<Row>
 									<Col className="error">
 										<h4 className="color-error mt-1 mb-0">Error</h4>
-										Failed to connect to Mikuia backend.
+										Something happened.
 									</Col>
 								</Row>
 							</div>
@@ -44,7 +44,41 @@ class DefaultLayout extends React.Component {
 
 		return (
 			<div>
-				<Test />
+				<Navbar dark color="mikuia" expand="md">
+					<Container>
+						<NavbarBrand href="/">
+							<img className="mb-auto" src="/images/logo.png" />
+							{/* Mikuia */}
+						</NavbarBrand>
+						<Nav className="mr-auto" navbar>
+							<NavItem>
+								<NavLink active href="/">Home</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="/">Poop</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="/">Butts</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="/">Dicks</NavLink>
+							</NavItem>
+						</Nav>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink href="/auth/twitch">Login with Twitch</NavLink>
+							</NavItem>
+						</Nav>
+					</Container>
+				</Navbar>
+				<Container>
+					<Row>
+						<Col md="12">
+							<h1>Headline</h1>
+							Something interesting.
+						</Col>
+					</Row>
+				</Container>
 			</div>
 		)
 	}
